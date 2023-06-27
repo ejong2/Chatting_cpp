123b2e1f17975f1ea033ee6c6e4142891a4f1a10
ejong2 <guyus5594@gmail.com>
2023-06-27 14:23:40 +0900

    패킷 타입 정의 #1

--- chatting_cpp_server/PacketEnums.h
+++ chatting_cpp_server/PacketEnums.h
@@ -1,5 +1,21 @@
 #pragma once
-class PacketEnums
+enum PacketID
 {
-};
+    ID_NONE,             // 패킷 아이디가 지정되지 않았을 경우
+    C2S_LOGIN_REQUEST,   // 클라이언트에서 서버로 로그인 요청
+    S2C_LOGIN_RESPONSE,  // 서버에서 클라이언트로 로그인 응답
 
+    C2S_SIGNUP_REQUEST,  // 클라이언트에서 서버로 회원가입 요청
+    S2C_SIGNUP_RESPONSE, // 서버에서 클라이언트로 회원가입 응답
+
+    C2S_LOGOUT,          // 클라이언트에서 서버로 로그아웃 요청
+
+    C2S_CHAT_MESSAGE,    // 클라이언트에서 서버로 채팅 메시지 전송 요청
+    S2C_CHAT_BROADCAST,  // 서버에서 클라이언트로 채팅 메시지 전송 (모든 채팅 참가자에게)
+
+    C2S_ROOM_CREATE,     // 클라이언트에서 서버로 채팅방 생성 요청
+    C2S_ROOM_JOIN,       // 클라이언트에서 서버로 채팅방 입장 요청
+    C2S_ROOM_LEAVE,      // 클라이언트에서 서버로 채팅방 퇴장 요청
+    C2S_ROOM_DELETE,     // 클라이언트에서 서버로 채팅방 삭제 요청
+    C2S_ROOM_USER_LIST   // 클라이언트에서 서버로 채팅방에 참가중인 사용자 리스트 요청
+};
--- chatting_cpp_server/PacketStructs.h
+++ chatting_cpp_server/PacketStructs.h
@@ -1,5 +1,89 @@
 #pragma once
-class PacketStructs
+
+#include <cstring>
+#include "PacketEnums.h"
+
+typedef unsigned char BYTE;
+
+struct Packet
 {
+    int size;
+    PacketID id;
+    BYTE data[255];
 };
+
+struct ResponsePacket : public Packet
+{
+    bool result; // Request 처리 결과를 나타냅니다. true: 성공, false: 실패
+};
+
+
+struct C2S_LOGIN_REQUEST : public Packet
+{
+    char userID[50];
+    char password[50];
+};
+
+struct S2C_LOGIN_RESPONSE : public ResponsePacket
+{
+    // 로그인 결과를 보여줄 추가 필드를 넣을 수 있습니다.
+};
+
+struct C2S_SIGNUP_REQUEST : public Packet
+{
+    char userID[50];
+    char password[50];
+};
+
+struct S2C_SIGNUP_RESPONSE : public ResponsePacket
+{
+    // 회원 가입 결과를 보여줄 추가 필드를 넣을 수 있습니다.
+};
+
+struct C2S_LOGOUT : public Packet
+{
+    char userID[50];
+};
+
+struct C2S_CHAT_MESSAGE : public Packet
+{
+    char userID[50];
+    char message[255];
+};
+
+struct S2C_CHAT_BROADCAST : public Packet
+{
+    char userID[50];
+    char message[255];
+};
+
+struct C2S_ROOM_CREATE : public Packet
+{
+    char userID[50];
+    char roomName[50];
+};
+
+struct C2S_ROOM_JOIN : public Packet
+{
+    char userID[50];
+    char roomName[50];
+};
+
+struct C2S_ROOM_LEAVE : public Packet
+{
+    char userID[50];
+    char roomName[50];
+};
+
+struct C2S_ROOM_DELETE : public Packet
+{
+    char userID[50];
+    char roomName[50];
+};
+
+struct C2S_ROOM_USER_LIST : public Packet
+{
+    char roomName[50];
+};
+
 
